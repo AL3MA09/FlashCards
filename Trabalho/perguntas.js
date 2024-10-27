d1948c1ddbcff1b8183e8eb688ffa0688a0cb4a6
AL3MA09 <137588732+AL3MA09@users.noreply.github.com>
2024-10-27 15:58:58 -0300

    Add files via upload

---
+++ Trabalho/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'Qual é o meio correto para se desenvolver um site online?',
+    'A maneira mais correta para fazer códigos de um site é o Visual Studio Code, mas uma maneira bem mais prática é o WordPress '
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o jogo mais bem vendido de todos os tempos?',
+    'Atualmente é o Tetris, com mais de 700 milhões de cópias vendidas.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual é a linguagem de programação mais complexa de se desenvolver um código?',
+    'Considerada uma das linguagens mais poderosas e versáteis, o C++ é conhecido por sua complexidade. Ele possui uma sintaxe complexa e exige um entendimento profundo de conceitos como ponteiros, alocação de memória e gerenciamento de recursos. '
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o Lançamento mais esperado do mundo dos Games de 2025?',
+    ' O mais aguardado de 2025 é o famoso Grand Theft Auto 6 (GTA 6) Apesar do Trailer mostrar que o jogo sairá em 2025, a empresa já disse que é possível que esse lançamento seja adiado. ;-; '
+)
